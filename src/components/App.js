cff8cd398f991a7ded99cc7b20491d8635968c94
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2019-07-31 18:24:18 -0300

    ENC : improve directories structure and set a reset style boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+            <p>Star Wars - The Front Revenge </p>
+		</div>
+	);
+}
+
+export default App;
