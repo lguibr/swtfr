43860a91809e7867f0c40636da2b27665d14f609
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2019-08-04 19:34:00 -0300

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import ThemeContainer from "./components/ThemeContainer";
-
-ReactDOM.render(<ThemeContainer />, document.getElementById("root"));
