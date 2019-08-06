d38432a8733b69cf8b7d08a813e6e0ad4eae28ab
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2019-08-06 02:03:24 -0300

    OTH

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,61 +6,70 @@ import ModelCard from "./modelCard/ModelCard";
 const style = {
 	Grid: {
 		maxWidth: "100%",
-		margin:"auto"
+		margin: "auto"
 	}
 };
 //  ModelCard Models
 //  ElementsCard ElementModels
 // 	PropCard ElementProps
 
-const isMostlyNumber = (string)=>{
-	let countNumber = 0 
-	console.warn(string)
+const isUrl = string => {
+	const http = "http";
+	return string.match(http);
+};
+
+const isMostlyNumber = string => {
+	let countNumber = 0;
+	console.warn(string);
 	for (let i = 0; i < string.length; i++) {
 		const char = string[i];
 
-		if(!isNaN(parseFloat(char))){
-			countNumber =+ 1
+		if (!isNaN(parseFloat(char))) {
+			countNumber = +1;
 		}
 	}
-	console.error(countNumber);
+	return countNumber > string.length / 2;
+};
 
-	return (countNumber>(string.length/2))
-}
-
-const getCardType = (propValue)=>{
-	if(typeof(propValue) == "number"){
-		return "is a number"
-	}
-	if(typeof(propValue) == "string" ){
+const getCardType = propValue => {
+	if (typeof propValue == "number") {
+		return "number";
+	} else if (typeof propValue == "string") {
 		if (isMostlyNumber(propValue)) {
-			return "é majoritariamente um numero";
+			return "mostlyNumber";
+		} else if (isUrl(propValue)) {
+			return "url"
+		} else {
+			return "string";
 		}
-		return("é string")
-	}else if (Array.isArray(propValue)){
-		return("é array")
+	} else if (Array.isArray(propValue)) {
+		return "array";
+	} else if (typeof propValue == "object") {
+		if (propValue == null) {
+			return "null";
+		} else {
+			return "object";
+		}
 	} else {
-		return ("ainda não identificado")
+		return "undefined";
 	}
-}
+};
 
 export class Main extends Component {
 	render() {
 		const { data } = this.props;
 
 		Object.keys(data).map((e, i) => {
-			console.log(e,":" ,data[e] ,":" , i);
-			console.error(getCardType(data[e]))
+			console.log(e, ":", data[e], ":", i);
+			console.error(getCardType(data[e]));
 		});
 
 		return (
-			<Grid
-				container
-				wrap="wrap"
-				style={style.Grid}
-				spacing={2}
-			>
+			<Grid container wrap="wrap" style={style.Grid} spacing={2}>
 				{Object.keys(data).map((e, i) => {
+					if (getCardType == null) {
+						return null;
+					}
 					return (
 						<Grid key={i} xs={6} sm={4} md={3} item>
 							<ModelCard
